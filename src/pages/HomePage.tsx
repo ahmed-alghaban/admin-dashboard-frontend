abc5a338aa3c491e381eff67782489233fc68eab
ahmed-alghaban <a.alghaban@outlook.com>
2025-08-07 11:27:58 +0300

    adding main layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,7 @@
-import { LandingNavbar } from '@/components/shared/LandingNavbar'
-
 const HomePage = () => {
     return (
-        <div>
-            <LandingNavbar />
+        <div className="bg-muted min-h-[calc(100vh-4rem)]">
+            <h1 className="text-4xl font-bold text-center pt-10">Home Page</h1>
         </div>
     )
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ProtectedRoute } from './ProtectedRoute'
 import LoginPage from '@/features/auth/pages/LoginPage'
 import HomePage from '@/pages/HomePage'
+import MainLayout from '@/components/shared/MainLayout'
 
 const Router = () => {
     return (
         <BrowserRouter>
             <div className="routes-container">
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
+                    <Route path="/" element={<MainLayout />}>
+                        <Route path="/" element={<HomePage />} index />
+                        <Route path="/login" element={<LoginPage />} />
+                    </Route>
                     <Route path="/app" element={<ProtectedRoute />} />
-                    <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </div>
         </BrowserRouter>
