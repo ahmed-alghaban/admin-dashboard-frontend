d7ab3a42bbf95c85014aaad3b79b5dcb8b7de5d3
ahmed-alghaban <a.alghaban@outlook.com>
2025-08-28 11:58:29 +0300

    cleanup: remove accidentally created file 3

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_APP_TITLE: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
